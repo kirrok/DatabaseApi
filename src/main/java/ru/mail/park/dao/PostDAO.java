f66a075f0cdb35ecf47d4511e792a9f73df37841
Sobolev Kirill <kirrok@mail.ru>
2016-11-21 21:31:20 +0300

    Post Dao

---
+++ src/main/java/ru/mail/park/dao/PostDAO.java
@@ -0,0 +1,23 @@
+package ru.mail.park.dao;
+
+import ru.mail.park.controllers.CustomResponse;
+
+import java.util.List;
+
+public interface PostDAO {
+    void truncateTable();
+
+    int count();
+
+    CustomResponse create(String postString);
+
+    CustomResponse details(String postId, List<String> related);
+
+    CustomResponse list(String forumShortName, String threadId, String since, String limit, String order);
+
+    CustomResponse removeOrRestore(String postString, String action);
+
+    CustomResponse update(String postString);
+
+    CustomResponse vote(String voteString);
+}
