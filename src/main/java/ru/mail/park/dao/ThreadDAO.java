d96498c60741b95ada0233d791f1555686a89821
Sobolev Kirill <kirrok@mail.ru>
2016-11-22 17:35:56 +0300

    thread DO

---
+++ src/main/java/ru/mail/park/dao/ThreadDAO.java
@@ -0,0 +1,35 @@
+package ru.mail.park.dao;
+
+import ru.mail.park.controllers.CustomResponse;
+
+import java.util.List;
+
+public interface ThreadDAO {
+    void truncateTable();
+
+    int count();
+
+    CustomResponse create(String threadString);
+
+    CustomResponse details(String threadId, List<String> related);
+
+    CustomResponse close(String threadString);
+
+    CustomResponse open(String threadString);
+
+    CustomResponse list(String forumShortName, String email, String since, String limit, String order);
+
+    CustomResponse remove(String threadString);
+
+    CustomResponse restore(String threadString);
+
+    CustomResponse subscribe(String subscribeString);
+
+    CustomResponse unsubscribe(String unsubscribeString);
+
+    CustomResponse update(String threadString);
+
+    CustomResponse vote(String voteString);
+
+    CustomResponse listPosts(String threadId, String sort, String since, String limit, String order);
+}
