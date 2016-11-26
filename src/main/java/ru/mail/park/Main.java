62f9aea584a4eba70dda1c500746ab621a4c199b
Sobolev Kirill <kirrok@mail.ru>
2016-11-26 20:46:27 +0300

    added Main

---
+++ src/main/java/ru/mail/park/Main.java
@@ -0,0 +1,17 @@
+package ru.mail.park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class Main {
+    public static DataSource connection;
+
+    public static void main(String[] args) throws Exception {
+        final Connector connector = new Connector();
+        connection = connector.createSource();
+        SpringApplication.run(Main.class, args);
+    }
+}
