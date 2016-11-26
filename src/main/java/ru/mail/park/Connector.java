631f5109f3196cc182b56354197fe2a4d39f747d
Sobolev Kirill <kirrok@mail.ru>
2016-11-26 20:41:14 +0300

    Connector

---
+++ src/main/java/ru/mail/park/Connector.java
@@ -0,0 +1,36 @@
+package ru.mail.park;
+
+import org.apache.commons.dbcp.ConnectionFactory;
+import org.apache.commons.dbcp.DriverManagerConnectionFactory;
+import org.apache.commons.dbcp.PoolableConnectionFactory;
+import org.apache.commons.dbcp.PoolingDataSource;
+import org.apache.commons.pool.impl.GenericObjectPool;
+
+import javax.sql.DataSource;
+
+public class Connector {
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+
+    public static final String URL = "jdbc:mysql://localhost:3306/TPForum?autoreconnect=true&useUnicode=yes&useSSL=false&characterEncoding=UTF-8";
+
+    public static final String USER = "root";
+
+    public static final String PASSWORD = "1";
+
+    public DataSource createSource() throws Exception
+    {
+        Class.forName(DRIVER).newInstance();
+        final GenericObjectPool     connectionPool = new GenericObjectPool();
+        connectionPool.setMaxActive(100);
+        final ConnectionFactory connectionFactory = new DriverManagerConnectionFactory(URL, USER, PASSWORD);
+
+        final PoolableConnectionFactory pcf = new PoolableConnectionFactory(
+                connectionFactory,
+                connectionPool,
+                null,
+                null,
+                false,
+                true);
+        return new PoolingDataSource(connectionPool);
+    }
+}
